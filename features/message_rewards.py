9fbe42f7bf9f635b6990b37d8d832e048f6a79ea
AndreMuhamed <kg7221996@gmail.com>
2024-08-17 15:31:49 +0300

    first message reward

--- features/message_rewards.py
+++ features/message_rewards.py
@@ -14,13 +14,32 @@ class MessageRewards(commands.Cog):
         user_id = str(message.author.id)
         data = self.load_data()
 
-        if user_id not in data:
-            data[user_id] = {"rewards": 0}
-        
-        data[user_id]["rewards"] += 1450
-        self.save_data(data)
+        # Проверяем, если пользователь еще не получил награду за первое сообщение
+        if user_id not in data or "rewarded" not in data[user_id]:
+            # Обновляем данные пользователя
+            if user_id not in data:
+                data[user_id] = {"rewards": 0}
+                
+            data[user_id]["rewards"] += 1450
+            data[user_id]["rewarded"] = True
+            self.save_data(data)
 
-        print(f"Added 5 coins to user {user_id}. Total: {data[user_id]['rewards']}")
+            # Отправляем сообщение в личку
+            try:
+                embed = disnake.Embed(
+                    title="Поздравления о награде!",
+                    description=f"Вы получили **награду** за ваше первое сообщение на сервере **Андрея Мухамеда**!",
+                ).add_field(
+                    name="> Награда:", value="```1450```", inline=False
+                ).set_footer(
+                    text="Спасибо, что присоединились к нам!"
+                ).set_thumbnail(
+                    url="https://cdn.discordapp.com/attachments/963534892082290688/1269283846956781578/626a7fb9f5861b9f.png"  # Замените на URL аватара вашего магазина
+                )
+
+                await message.author.send(embed=embed)
+            except disnake.Forbidden:
+                print(f"Не удалось отправить личное сообщение пользователю {message.author.id}")
 
         await self.bot.process_commands(message)
 
@@ -44,3 +63,5 @@ class MessageRewards(commands.Cog):
 
 def setup(bot: commands.Bot):
     bot.add_cog(MessageRewards(bot))
+
+
