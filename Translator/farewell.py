4e23685c7119f86b47ea1d8f72e2025616d33675
Muhamed Blogs <kg7221996@gmail.com>
2024-10-02 22:20:47 +0300

    text

--- Translator/farewell.py
+++ Translator/farewell.py
@@ -2,15 +2,15 @@ translations = {
     "ru": {
         "farewell_title": "До свидания пользователь!",
         "farewell_message": "Жаль видеть тебя покидающим нас, {member}. Надеемся, что тебе понравилось здесь и ты вернёшься в будущем.",
-        "recommendation": "Ты всегда можешь подписаться на проекты **Андрея Мухамеда** на YouTube, нажав на кнопки ниже. **Будем рады видеть тебя снова!**",
+        "recommendation": "Ты всегда можешь подписаться на проекты **Андрея Мухамеда** в YouTube, нажав на ссылку ниже. **Будем рады видеть тебя снова!**",
         "links": "Ссылки на YouTube-канали:",
         "footer_text": "Береги себя!",
         "support_button": "Поддержать создателя",
     },
     "uk": {
         "farewell_title": "До побачення користувач!",
-        "farewell_message": "Шкода бачити тебе, {member}. Сподіваємось, що тобі сподобалося тут і ти повернешся до майбутнього.",
-        "recommendation": "Ти завжди можеш передплатити проекти **Андрея Мухамеда** на YouTube, натиснувши на кнопки нижче. **Будемо раді бачити тебе знову!**",
+        "farewell_message": "Шкода прощатися з тобою, {member}. Сподіваємось, що тобі сподобалося тут і ти повернешся до майбутнього.",
+        "recommendation": "Ти завжди можеш подписатися на проекти **Андрея Мухамеда** в YouTube, натиснувши на посилання нижче. **Будемо раді бачити тебе знову!**",
         "links": "Посилання на YouTube-канали:",
         "footer_text": "Бережи себе!",
         "support_button": "Підтримати творця",
@@ -18,7 +18,7 @@ translations = {
     "en": {
         "farewell_title": "Goodbye user!",
         "farewell_message": "We're sad to see you go, {member}. We hope you enjoyed your stay here and will come back in the future.",
-        "recommendation": "You can always subscribe to **Andrey Muhameda** projects on YouTube by clicking the buttons below. **We'll be glad to see you again!**",
+        "recommendation": "You can always subscribe to the projects of **Andrey Muhamed** on YouTube by clicking on the link below. **We will be glad to see you again!**",
         "links": "Links to YouTube-channels:",
         "footer_text": "Take care!",
         "support_button": "Support the creator",
--- Translator/welcome.py
+++ Translator/welcome.py
@@ -4,7 +4,7 @@ translations = {
     "ru": {
         "welcome_title": "Добро пожаловать на сервер!",
         "welcome_message": "Привет, {member}! Ты находишься на одном из проектов **Андрея Мухамеда**.",
-        "recommendation": "Рекомендуем подписаться на его другие проекты на YouTube, нажав на кнопки ниже под этим сообщением. **Заранее благодарим!**",
+        "recommendation": "Рекомендуем подписаться на другие проекты на YouTube, нажав на ссылку ниже. **Заранее спасибо!**",
         "links": "Ссылки на YouTube-канали:",
         "footer_text": "Спасибо за вашу будущую активность!",
         "support_button": "Поддержать создателя",
@@ -12,7 +12,7 @@ translations = {
     "uk": {
         "welcome_title": "Ласкаво просимо на сервер!",
         "welcome_message": "Привіт, {member}! Ти знаходишся на одному з проектів **Андрея Мухемеда**.",
-        "recommendation": "Рекомендуємо підписатися на його інші проекти на YouTube, натиснувши на кнопки нижче під цим повідомленням. **Заздалегідь дякуємо!**",
+        "recommendation": "Рекомендуємо підписатися на його інші проекти на YouTube, натиснувши на посилання нижче. **Заздалегідь дякуємо!**",
         "links": "Посилання на YouTube-канали:",
         "footer_text": "Дякуємо за вашу майбутню активність!",
         "support_button": "Підтримати творця",
@@ -20,7 +20,7 @@ translations = {
     "en": {
         "welcome_title": "Welcome to the server!",
         "welcome_message": "Hi, {member}! You are on one of **Andrey Muhameda** projects.",
-        "recommendation": "We recommend subscribing to his other projects on YouTube by clicking the buttons below this message. **Thank you in advance!**",
+        "recommendation": "We encourage you to subscribe to his other projects on YouTube by clicking on the links below. **Thank you in advance!**",
         "links": "Links to YouTube-channels:",
         "footer_text": "Thank you for your future activity!",
         "support_button": "Support the creator",
